c8efa40ef542a0ffa76547b1c97d3580a6b64981
andrinwinzap <AndrinWinzap@proton.me>
2025-02-25 13:38:38 +0100

    switch to chromium

--- Install-Web.sh
+++ Install-Web.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # Update & upgrade system
 sudo apt update && sudo apt upgrade -y
 
-# Install minimal X server & Falkon browser
-sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox falkon git -y
+# Install X server, Openbox, and Chromium (instead of Falkon)
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser git -y
 
 # Clone the web application repository
 git clone https://github.com/aloago/Fasnacht.git ~/Fasnacht
@@ -10,10 +12,24 @@ git clone https://github.com/aloago/Fasnacht.git ~/Fasnacht
 # Create Openbox config directory if it doesn't exist
 mkdir -p ~/.config/openbox
 
-# Create the autostart file for Openbox (corrected syntax)
-echo 'falkon --kiosk file://'"$HOME"'/Fasnacht/Web_2.0/index.html' > ~/.config/openbox/autostart
+# Create the autostart file for Chromium in kiosk mode
+echo 'chromium-browser \
+--kiosk \
+--incognito \
+--noerrdialogs \
+--disable-infobars \
+--disable-session-crashed-bubble \
+--disable-features=TranslateUI \
+--disable-pinch \
+--block-new-web-contents \
+file://'"$HOME"'/Fasnacht/Web_2.0/index.html' > ~/.config/openbox/autostart
 
-# Make sure the autostart script is executable
+# Add screen saver/power management disable commands
+echo 'xset s off' >> ~/.config/openbox/autostart
+echo 'xset -dpms' >> ~/.config/openbox/autostart
+echo 'xset s noblank' >> ~/.config/openbox/autostart
+
+# Make the autostart script executable
 chmod +x ~/.config/openbox/autostart
 
 # Ensure startx runs on login
