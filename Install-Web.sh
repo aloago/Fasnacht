41edfed490310c2e1d4b978e4d427034961beb51
andrinwinzap <AndrinWinzap@proton.me>
2025-02-25 14:15:52 +0100

    add unclutter

--- Install-Web.sh
+++ Install-Web.sh
@@ -4,7 +4,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install X server, Openbox, and Chromium (instead of Falkon)
-sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser git -y
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser git unclutter -y
 
 # Clone the web application repository
 git clone https://github.com/aloago/Fasnacht.git ~/Fasnacht
@@ -28,6 +28,9 @@ file://'"$HOME"'/Fasnacht/Web_2.0/index.html' > ~/.config/openbox/autostart
 echo 'xset s off' >> ~/.config/openbox/autostart
 echo 'xset -dpms' >> ~/.config/openbox/autostart
 echo 'xset s noblank' >> ~/.config/openbox/autostart
+
+# Add unclutter command
+echo 'unclutter -idle 0 &' >> ~/.config/openbox/autostart
 
 # Make the autostart script executable
 chmod +x ~/.config/openbox/autostart
